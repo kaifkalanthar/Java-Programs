4ced06bac529510460053eb486d0d36ff4771314
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2021-04-02 21:14:36 +0400

    Commit as java file

---
+++ Comfortable loop.java
@@ -0,0 +1,15 @@
+package com.Day3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner (System.in);
+        int Number =input.nextInt();
+        for (int i =0;i<Number;i++){
+            System.out.println(i);
+        }
+    }
+}
+
